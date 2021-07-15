f1533ed53b4da1cf7dd6d4408a8ba1697cbe9f76
Thiago Trannin <51510921+thinety@users.noreply.github.com>
2021-07-15 19:16:37 -0300

    Trabalho 3 - contagem em milissegundos

--- old/t3.c
+++ old/t3.c
@@ -3,38 +3,50 @@
 #include <stdbool.h>
 
 /**
- * A lógica desse programa funciona da mesma forma que o programa que conta
- * segundos, mas aqui são contados milissegundos. Portanto, não há diferença
- * macroscopicamente perceptível na simulação.
+ * Algumas considerações sobre a lógica do programa:
  *
- * Novamente, supõe-se que o clock de I/O é 1 MHz. Como queremos contar
- * milissegundos utilizando um timer de 8 bits, utilizamos um prescaler de 8.
- * Nessa configuração, 125 ticks equivalem a 1 milissegundo (1 MHz / 8 = 125 kHz).
- * Assim, o TOP do timer é 125-1 = 124.
+ * Supõe-se que o clock de I/O é 1 MHz (oscilador interno de 8 MHz,
+ * com o divisor de clock por 8 habilitado). Queremos contar segundos,
+ * e como o maior prescaler disponível é 1024, precisamos contar no
+ * mínimo 976 ticks (1 Mhz / 1024 = 976.5625 Hz). Esse valor não cabe
+ * em 8 bits, portanto é necessário utilizar o Timer1 com precisão de
+ * 16 bits.
+ *
+ * Nota: os outros timers poderiam ser utilizados caso a contagem
+ * fosse baseada em milissegundos, mas isso introduziria complexidade
+ * desnecessária, já que seria necessário converter de milissegundos para
+ * segundos dentro do loop principal do programa. Assim, optou-se por fazer
+ * a contagem em segundos, e utilizar o timer de 16 bits.
+ *
+ * O menor prescaler que pode ser utilizado é 64, resultando em 15625
+ * ticks necessários (1 MHz / 64 = 15625 Hz). Portanto, o TOP do timer é
+ * 15625-1 (ele conta de 0 até 15624, totalizando 15625 ticks), e é utilizado
+ * o modo de operação CTC (Clear Timer on Compare Match), o que reinicia o
+ * timer quando ele atinge o TOP.
  */
 
 // Definição do TOP do timer
-#define TIMER_TOP (125 - 1)
+#define TIMER_TOP (15625 - 1)
 
-// Variável para salvar o número de milissegundos atual
-volatile uint16_t time = 0;
+// Variável para salvar o número de segundos atual
+volatile uint8_t time = 0;
 // Inicialmente o relógio está em modo crescente.
 // Pode ficar decrescente quando `step` é -1
-volatile uint16_t step = 1;
+volatile uint8_t step = 1;
 
 // Interrupção que é disparada toda vez que o timer atinge TOP
-ISR(TIMER0_COMPA_vect) {
+ISR(TIMER1_COMPA_vect) {
     // Incrementa/decrementa o número de segundos atual
     time += step;
 
     // Verifica o caso de overflow positivo
-    if (time == 60000) {
+    if (time == 60) {
         time = 0;
     }
 
     // Verifica o caso de overflow negativo
-    if (time == 65535) {
-        time = 59999;
+    if (time == 255) {
+        time = 59;
     }
 }
 
@@ -60,10 +72,10 @@ ISR(PCINT1_vect) {
     ) {
         // Desabilita/habilita o clock do timer
         if (clock_running) {
-            TCCR0B &= ~(1<<1);
+            TCCR1B &= ~((1<<1) | (1<<0));
             clock_running = false;
         } else {
-            TCCR0B |= 1<<1;
+            TCCR1B |= (1<<1) | (1<<0);
             clock_running = true;
         }
     }
@@ -75,11 +87,11 @@ ISR(PCINT1_vect) {
     ) {
         // Inverte o sentido de contagem do relógio
         if (clock_ascending) {
-            TCNT0 = TIMER_TOP - TCNT0;
+            TCNT1 = TIMER_TOP - TCNT1;
             step = -1;
             clock_ascending = false;
         } else {
-            TCNT0 = TIMER_TOP - TCNT0;
+            TCNT1 = TIMER_TOP - TCNT1;
             step = 1;
             clock_ascending = true;
         }
@@ -91,7 +103,7 @@ ISR(PCINT1_vect) {
         && !reset_button_was_pressed
     ) {
         // Reseta o estado do timer e do relógio
-        TCNT0 = 0;
+        TCNT1 = 0;
         time = 0;
     }
 
@@ -143,21 +155,21 @@ int main() {
     PORTD = 0b00000000; // Data Register D
 
 
-    // Configuração do Timer 0, utilizado para a contagem dos milissegundos
+    // Configuração do Timer 1, utilizado para a contagem dos segundos
 
     // Modo de operação CTC (Clear Timer on Compare Match)
-    // Prescaler de 8
-    TCCR0A = 0b00000010; // Timer/Counter 0 Control Register A
-    TCCR0B = 0b00000010; // Timer/Counter 0 Control Register B
+    // Prescaler de 64
+    TCCR1A = 0b00000000; // Timer/Counter 1 Control Register A
+    TCCR1B = 0b00001011; // Timer/Counter 1 Control Register B
 
     // Habilita a interrupção quando o timer atinge TOP
-    TIMSK0 = 0b00000010; // Timer/counter 0 Interrupt MaSK register
+    TIMSK1 = 0b00000010; // Timer/counter 1 Interrupt MaSK register
 
     // Timer começa em 0
-    TCNT0 = 0; // Timer/CouNTer 0
+    TCNT1 = 0; // Timer/CouNTer 1
 
     // TOP do timer é `TIMER_TOP`
-    OCR0A = TIMER_TOP; // Output Compare Register 0 A
+    OCR1A = TIMER_TOP; // Output Compare Register 1 A
 
 
     // Configuração das interrupções dos botões
@@ -174,7 +186,7 @@ int main() {
 
 
     while (true) {
-        uint8_t current_time = time / 1000;
+        uint8_t current_time = time;
         uint8_t time_digit_0 = current_time % 10;
         uint8_t time_digit_1 = current_time / 10;
 
